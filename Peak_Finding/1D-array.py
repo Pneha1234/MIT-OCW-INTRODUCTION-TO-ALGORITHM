926d26cbefea73a1f9c200df2f4b6fccdaf4d053
Pneha <neha.pandey@debajyotigroup.com>
2020-04-25 22:26:57 +0545

    solution to 1D-array peak finding problem

---
+++ Peak_Finding/1D-array.py
@@ -0,0 +1,34 @@
+# question: to find the peak of one dimensional array
+# statement: a peak is any element b such that b-1<= b >=b+1,
+# edge_case definition: a peak is any element b such that b>=b-1 or b>=b+1
+# suggested method:
+#  if b[n/2]<b[n/2 -1] then only look at left half 1..n/2
+#  elif b[n/2]< b[n/2+1] then only look at right half n/2 +1
+# else n/2 is the peak
+
+# solution
+def find_a_peak(array):
+    # size = int(input('Enter size of array: '))
+    # array = [int(input('Enter a value: ')) for _ in range(size)]
+    if len(array) == 0:
+        return None
+
+    if len(array) == 2:
+        return array[1]
+
+    half = len(array) // 2
+
+    if array[half - 1] < array[half] and array[half] > array[half + 1]:
+        return array[half]
+
+    if array[half - 1] >= array[half]:
+        peak = find_a_peak(array[0:half])
+        if peak:
+            return peak
+
+    if half >= 1 and array[half + 1] >= array[half]:
+        peak = find_a_peak(array[half:len(array)])
+        if peak:
+            return peak
+
+
